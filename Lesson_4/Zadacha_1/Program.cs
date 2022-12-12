f4af499c3f4b629875aa4efcd489d83cbe2ac301
mramorov <ygavva@sanatoriysochi.ru>
2022-12-12 22:42:09 +0300

    main

---
+++ Lesson_4/Zadacha_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+int exponentiation = Exponentiation(numA, numB);
+int Exponentiation(int numA, int numB)
+{
+  int result = 1;
+  for(int i=1; i <= numB; i++){
+    result = result * numA;
+  }
+    return result;
+}
+Console.WriteLine("Ответ: " + exponentiation);
