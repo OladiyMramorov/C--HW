4ace1bfa073523b9ba12fcfaf11cbe070c88af4e
mramorov <ygavva@sanatoriysochi.ru>
2022-12-20 19:04:11 +0300

    main

---
+++ Lesson_5/Zadacha_2/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите размер массива:");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] num = new int[size];
+FillArrayRandomNumbers(num);
+Console.Write("Массив - ");
+PrintArray(num);
+int sum = 0;
+
+for (int z = 0; z < num.Length; z+=2)
+    sum = sum + num[z];
+
+    Console.WriteLine($"Всего чисел в массиве {num.Length}, сумма элементов на нечётных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] num)
+{
+    for(int i = 0; i < num.Length; i++)
+        {
+            num[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] num)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < num.Length; i++)
+        {
+            Console.Write(num[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
---
+++ Lesson_5/Zadacha_3/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] num = new double[size];
+FillArrayRandomNumbers(num);
+Console.Write("Массив - ");
+PrintArray(num);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+for (int z = 0; z < num.Length; z++)
+{
+    if (num[z] > max)
+        {
+            max = num[z];
+        }
+    if (num[z] < min)
+        {
+            min = num[z];
+        }
+}
+Console.WriteLine($"Разница между максимальным и минимальным значением массива = {max - min}");
+void FillArrayRandomNumbers(double[] num)
+{
+    for(int i = 0; i < num.Length; i++)
+        {
+            num[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] num)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < num.Length; i++)
+        {
+            Console.Write(num[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
