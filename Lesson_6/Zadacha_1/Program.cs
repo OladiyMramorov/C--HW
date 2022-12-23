938e4bf201f8bd4f0813669cb0160d80b2ceaca9
mramorov <ygavva@sanatoriysochi.ru>
2022-12-23 23:35:38 +0300

    main

--- Lesson_6/Zadacha_1/Program.cs
+++ Lesson_6/Zadacha_1/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.Write("Введите колличество чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите числа через запятую: ");
+List<int> numbers = Console.ReadLine().Split(',').Select(Int32.Parse).ToList();
+int sum = 0;
+for (int i = 0; i < m; i++)
+{
+    if (numbers[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"количество значений больше 0 = {sum}");
