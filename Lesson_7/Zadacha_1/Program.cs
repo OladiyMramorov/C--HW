87944025fed4ec79202b7d0ddd7d33618aba7db5
mramorov <ygavva@sanatoriysochi.ru>
2023-01-02 16:07:23 +0300

    main

--- Lesson_7/Zadacha_1/Program.cs
+++ Lesson_7/Zadacha_1/Program.cs
@@ -2,22 +2,23 @@
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите n: ");
 int n = Convert.ToInt32(Console.ReadLine());
-double[,] array = FillRandomArray(m, n);
-PrintTwoDimenArray(array);
-double[,] FillRandomArray(int m, int n)
+double[,] array = RandomArray(m, n);
+PrintArray(array);
+double[,] RandomArray(int m, int n)
 {
     double[,] array = new double[m,n];
-    Random rnd = new Random();
+    Random random = new Random();
     for (int i = 0; i < m; i++)
     {
         for(int j = 0; j < n; j++)
         {
-            array[i,j] = Math.Round(rnd.NextDouble() * 10, 2);
+            array[i,j] = Math.Round(random.NextDouble() * 10, 2);
         }
     }
     return array;
 }
-void PrintTwoDimenArray(double[,] array)
+
+void PrintArray(double[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
