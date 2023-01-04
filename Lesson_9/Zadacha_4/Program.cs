dedb0cdf9db1b02708fd8828f45efcccb8657c49
mramorov <ygavva@sanatoriysochi.ru>
2023-01-05 00:13:36 +0300

    main

--- Lesson_9/Zadacha_4/Program.cs
+++ Lesson_9/Zadacha_4/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿Console.Write("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] arr = new int[m, n];
+FillArray(arr);
+PrintArray(arr);
+int[,] FillArray(int[,] arr)
+{
+    int temp = 1;
+    int i = 0;
+    int j = 0;
+    while (temp <= arr.GetLength(0) * arr.GetLength(1))
+    {
+        arr[i, j] = temp;
+        temp++;
+        if (i <= j + 1 && i + j < arr.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= arr.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > arr.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
