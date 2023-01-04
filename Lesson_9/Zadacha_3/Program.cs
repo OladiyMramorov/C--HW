e74cc8b64ce9b6f371f76d956bd2c00d60226a40
mramorov <ygavva@sanatoriysochi.ru>
2023-01-04 14:29:25 +0300

    main

--- Lesson_9/Zadacha_3/Program.cs
+++ Lesson_9/Zadacha_3/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿Console.Write("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (m < 0 || n < 0)
+{
+    Console.WriteLine("Одно или оба числа отрицательные");
+}
+else
+{
+    Console.WriteLine($"A({m},{n}) = {FunctionAckerman(m, n)}");
+}
+
+int FunctionAckerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (m > 0 && n == 0)
+    {
+        return FunctionAckerman(m - 1, 1);
+    }
+    else
+    {
+        return FunctionAckerman(m - 1, FunctionAckerman(m, n - 1));
+    }
+}
+
