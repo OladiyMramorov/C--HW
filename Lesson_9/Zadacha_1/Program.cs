246d58e1724b212a36eab0f300217f3c6d1b455c
mramorov <ygavva@sanatoriysochi.ru>
2023-01-04 14:04:03 +0300

    main

--- Lesson_9/Zadacha_1/Program.cs
+++ Lesson_9/Zadacha_1/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Console.Write("Введите N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write(NaturalNumbersBetweenNandOne(num));
+int NaturalNumbersBetweenNandOne(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        Console.Write(n + " ");
+        return NaturalNumbersBetweenNandOne(n - 1);
+    }
+
+}
