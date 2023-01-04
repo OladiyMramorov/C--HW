69862b0c049ba712ae0ac465bd11b4e32d837a8c
mramorov <ygavva@sanatoriysochi.ru>
2023-01-04 14:15:28 +0300

    main

--- Lesson_9/Zadacha_2/Program.cs
+++ Lesson_9/Zadacha_2/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int sum;
+if (n<m)
+{
+    sum = SumNaturalElements(n, m);
+}
+else
+{
+    sum = SumNaturalElements(m, n);
+}
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {n} до {m} = {sum}");
+
+int SumNaturalElements(int n, int m)
+{
+    if (n <= m){
+        return n+SumNaturalElements(n+1,m);
+    }
+    else{
+        return 0;
+    }
+}
