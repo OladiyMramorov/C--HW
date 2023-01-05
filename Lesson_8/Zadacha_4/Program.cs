8de2c514379f534459a9e48c3f1f1ae1b80eabde
mramorov <ygavva@sanatoriysochi.ru>
2023-01-05 15:43:50 +0300

    main

--- Lesson_8/Zadacha_4/Program.cs
+++ Lesson_8/Zadacha_4/Program.cs
@@ -5,52 +5,44 @@
 Но хотелось бы чтобы вставляли эл-т, который уникальный и не присутствует в массиве. 
 Если вставлять эл-т и проверять его только после вставки, то проходов по массиву может быть очень много.
 */
-Console.WriteLine("Введите размер массива m*n (но не более чем 9*10)");
+
 Console.Write("Введите m: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите n: ");
 int n = Convert.ToInt32(Console.ReadLine());
-int[,] arr = new int[m, n];
-FillArray(arr);
+int[] sizeArray = {m, n};
+int[,] arr = FillArray(sizeArray);
 PrintArray(arr);
-GetNumbers(arr);
-Console.WriteLine();
 
-void FillArray(int[,] arr)
+int[,] FillArray(int[] sizeArray)
 {
+    int[,] array = new int[sizeArray[0], sizeArray[1]];    
     Random random = new Random();
-    for (int i = 0; i < arr.GetLength(0); i++)
+    array[0,0] = random.Next(1,10);    
+    int tmp = 0;
+    for (int i = 0; i < sizeArray[0]; i++)
     {
-        for (int j = 0; j < arr.GetLength(1); j++)
+        for (int j = 0; j < sizeArray[1]; j++)
         {
-            arr[i, j] = random.Next(10, 100);
+            if (i == 0 && j == 0) continue;
+            tmp = random.Next(1, 100);           
+            if (CheckDuplicate(array, tmp)) j--;            
+            else array[i, j] = tmp;
         }
     }
+    return array;
 }
 
-void GetNumbers(int[,] arr)
-{
+bool CheckDuplicate(int[,] arr, int num)
+{   
     for (int i = 0; i < arr.GetLength(0); i++)
     {
         for (int j = 0; j < arr.GetLength(1); j++)
         {
-            for (int l = 0; l < arr.GetLength(0); l++)
-            {
-                for (int k = 0; k < arr.GetLength(1); k++)
-                {
-                    if (arr[i, j] == arr[l, k])
-                    {
-                        if (k != j && i != l)
-                        {
-                            arr[i, j] = new Random().Next(10, 100);
-                            GetNumbers(arr);
-                        }
-                    }
-                }
-            }
-        }
-
+            if (arr[i,j] == num) return true;
+        }        
     }
+    return false;    
 }
 
 void PrintArray(int[,] arr)
